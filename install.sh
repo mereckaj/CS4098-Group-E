b05b15f54bbe3b87bb2d738c5da427ab4f288f5a
Julius Mereckas <mereckaj@tcd.ie>
2016-02-03 23:58:44 +0000

    fixed OS check

--- install.sh
+++ install.sh
@@ -13,15 +13,13 @@ fi
 # Check that platform is what the client asked for
 
 CHECK_OS="lsb_release -s -d"
-OS_VERSION=eval $CHECK_OS
+OS_VERSION=$(lsb_release -s -d)
 echo "OS: " $OS_VERSION
-if [[ $OS_VERSION != *"Ubuntu 14.04"]]; then
+if [[ $OS_VERSION != *"Ubuntu 14.04"* ]]
+then
 	echo "You are running: " $OS_VERSION " program is made for Ubuntu 14.04"
 	exit 1
-else
-	echo "gucci"
 fi
-exit 1
 
 
 # Check if python3.5 is installed
