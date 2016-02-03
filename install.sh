cf29b885cf778fbac18a978dd1988d2af2089f37
Julius Mereckas <mereckaj@tcd.ie>
2016-02-03 23:52:00 +0000

    Added checking for sudo and correct OS

--- install.sh
+++ install.sh
@@ -4,7 +4,25 @@ echo "[Install]"
 
 # Check if script is run with sudo
 
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root"
+   echo "Run \"sudo ./install.sh\""
+   exit 1
+fi
+
 # Check that platform is what the client asked for
+
+CHECK_OS="lsb_release -s -d"
+OS_VERSION=eval $CHECK_OS
+echo "OS: " $OS_VERSION
+if [[ $OS_VERSION != *"Ubuntu 14.04"]]; then
+	echo "You are running: " $OS_VERSION " program is made for Ubuntu 14.04"
+	exit 1
+else
+	echo "gucci"
+fi
+exit 1
+
 
 # Check if python3.5 is installed
 
@@ -15,7 +33,7 @@ echo "[Install]"
 pip install virtualenv
 virtualenv -p /usr/bin/python3.5 venv
 
-VE=`echo $VIRTUAL_ENV`
+VE="echo $VIRTUAL_ENV"
 if [ -z $VE ]; then
 	# The virtualenv variable is null, so we are not in 
 	# an active virtual environment. 
