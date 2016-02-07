35d33d3ee07302d0e1974d23532869562137edcd
Julius Mereckas <mereckaj@tcd.ie>
2016-02-07 20:35:58 +0000

    Remvoed debug info

--- src/static/js/editor.js
+++ src/static/js/editor.js
@@ -1,19 +1,14 @@
-console.log("Loaded editor.js");
 function submitData(path){
 	var data = editor.getSession().getValue();
-	console.log("Got data: " + data);
 	$.ajax({
 		type: "POST",
 		url: path,
 		data: { code: data},
 		success: function(d){
-			console.log("Got reply from server: " + d);
 			// Take the reply and make it the new document now.
 			document.open();
 			document.write(d);
 			document.close();
-			// document = d;
-			console.log("set document");
 		}
 	});
 }
