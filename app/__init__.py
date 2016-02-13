d6430c2dd253ddf87c0c64cb40f2af5ebe760a2b
Julius Mereckas <mereckaj@tcd.ie>
2016-02-13 13:54:57 +0000

    auth

--- app/__init__.py
+++ app/__init__.py
@@ -24,10 +24,7 @@ def create_app(config_name):
 		# User authentication information
 		username = db.Column(db.String(50), nullable=False, unique=True)
 		password = db.Column(db.String(255), nullable=False, server_default='')
-		reset_password_token = db.Column(db.String(100), nullable=False, server_default='')
-		# User email information
-		email = db.Column(db.String(255), nullable=False, unique=True)
-		confirmed_at = db.Column(db.DateTime())
+
 		# User information
 		active = db.Column('is_active', db.Boolean(), nullable=False, server_default='0')
 		first_name = db.Column(db.String(100), nullable=False, server_default='')
