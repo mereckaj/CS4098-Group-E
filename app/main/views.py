96050084e6e43e6ecbed155b43bf5612a92a5bc6
bengyrules <benbclear@gmail.com>
2016-02-16 18:04:41 +0000

    some auth stuff done, mostly working, need to get button working

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,23 @@
-from flask import render_template, url_for, request, session
+from flask import Flask, render_template, url_for, request, session
 from . import main
 from .runCode import pmlchecker
 from .. import db
+from flask_oauthlib.client import OAuth, OAuthException
+
+oauth = OAuth()
+
+FACEBOOK_APP_ID = '188477911223606'
+FACEBOOK_APP_SECRET = '621413ddea2bcc5b2e83d42fc40495de'
+
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
 
 # Result page
 @main.route("/result/",methods=["POST"])
@@ -19,3 +35,34 @@ def root_post():
 @main.route("/",methods=["GET"])
 def root_get():
 	return render_template("pmlcheck_form.html")
+
+# #facebook login page
+@main.route('/login')
+def login():
+    callback = url_for(
+        'facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True
+    )
+    return facebook.authorize(callback=callback)
+
+@main.route('/login/authorized')
+def facebook_authorized():
+    resp = facebook.authorized_response()
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    if isinstance(resp, OAuthException):
+        return 'Access denied: %s' % resp.message
+
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
