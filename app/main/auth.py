ef17ed2c07b88be60a5e57940fac660e2171aa4c
Julius Mereckas <mereckaj@tcd.ie>
2016-02-21 17:05:59 +0000

    removed unused file

--- app/main/auth.py
+++
@@ -1,9 +0,0 @@
-@login_manager.user_loader
-def load_user(userid):
-	user = User.query.get(int(userid))
-	if user:
-		return user
-
-@facebook.tokengetter
-def get_facebook_oauth_token():
-	return session.get('oauth_token')
