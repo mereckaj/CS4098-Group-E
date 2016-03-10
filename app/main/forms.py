4b1012e5ca502238a9cb0eaaa13615e4ef6ec026
Julius Mereckas <mereckaj@tcd.ie>
2016-03-10 16:33:38 +0000

    form edits to print better messages

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,20 +4,35 @@ from wtforms.validators import Required, Email, Length,EqualTo, Length, Regexp
 
 
 class RegisterForm(Form):
-	first_name = StringField('First Name',validators=[Required(),Length(min=1,max=64,message="First name is too long, we only need your first name")])
-	last_name = StringField('Last Name',validators=[Required()])
-	password = PasswordField('Password', [
+	first_name = StringField('First Name',validators=[
 		Required(),
-		EqualTo('confirm', message='Passwords must match')
-		# Removed to ease testing
+		Length(min=1,max=64,
+			message="First name must be between 1 and 64 characters long")
+	])
+	last_name = StringField('Last Name',validators=[
+		Required(),
+		Length(min=1,max=64,
+			message="Last name must be between 1 and 64 characters long")
+	])
+	password = PasswordField('Password', validators=[
+		Required(),
+		EqualTo("confirm", message="Passwords must match")
+		Length(min=8,max=64,
+			message="Password must be between 8 and 64 characters long"),
 		# Regexp("(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*()]*$",
 		# 	message="Password is too weak, make sure it meets all of the required conditions")
 	])
 	confirm = PasswordField('Repeat Password')
-	email = StringField('Email Address', [Length(min=6, max=64)])
+	email = StringField('Email Address', validators=[
+		Length(min=6, max=64,
+			message = "Email must be between 6 and 64 characters long")
+	])
 	submit = SubmitField("Register")
 
 class LoginForm(Form):
-	email = StringField('Email Address', [Length(min=6, max=64)])
-	password = PasswordField('Password', [Required()])
+	email = StringField('Email Address', validators=[
+		Length(min=6, max=64,
+			message="Email must be between 6 and 64 characters long")
+	])
+	password = PasswordField('Password', validators=[Required()])
 	submit = SubmitField("Login")
