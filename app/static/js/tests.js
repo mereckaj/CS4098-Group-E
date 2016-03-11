68d9434dbc19b5e423756080f511df2af31e61ac
Julius Mereckas <mereckaj@tcd.ie>
2016-03-11 16:23:21 +0000

    Added client side testing preperation

---
+++ app/static/js/tests.js
@@ -0,0 +1,19 @@
+"use strict"
+function runAllTests(){
+	// test_sample();
+}
+
+/*
+	Template for a test, rename your test function and add it to runALlTests
+*/
+function test_sample(){
+	/*
+		Replace TEMPLATE with a short description of what your test is doing
+	*/
+	console.log("Testing: TEMPLATE")
+
+
+	// print one of the results below
+	console.log("Result: PASS");
+	console.log("Result: FAIL");
+}
